af12f665868f2a85b04598aa732cbc2ce348a96e
Gichohi <njogugichohi56@gmail.com>
2021-06-19 19:21:15 +0300

    fixed layout module bug

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import Layout, { siteTitle } from '../components/Layout'
+import Layout, { siteTitle } from '../components/Layout.js'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 import Date from '../components/date'
